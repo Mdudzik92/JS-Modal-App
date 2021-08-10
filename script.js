6f094ba04b1cfae573083f14b4cdd0ea969472d3
Matt Dudzik <mdudzik92@gmail.com>
2021-08-10 01:26:18 -0500

    Refactored with new functional variables, closing modals on click or clicking the overlay

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-	btnsOpenModal[i].addEventListener("click", () => {
-		console.log("Button clicked");
-		modal.classList.remove("hidden");
-		overlay.classList.remove("hidden");
-	});
+const openModal = () => {
+	console.log("Button clicked");
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
 
-btnCloseModal.addEventListener("click", () => {
+const closeModal = () => {
 	modal.classList.add("hidden");
 	overlay.classList.add("hidden");
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+	btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
