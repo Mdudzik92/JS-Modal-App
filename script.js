db3532ef44b0991c0e821966a51bd750f1bc7078
Matt Dudzik <mdudzik92@gmail.com>
2021-08-10 01:44:54 -0500

    Added event listener to the document with conditional for closing the modal on keydown of esc key

--- script.js
+++ script.js
@@ -18,5 +18,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 	btnsOpenModal[i].addEventListener("click", openModal);
 
 btnCloseModal.addEventListener("click", closeModal);
-
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+		closeModal();
+	}
+});
